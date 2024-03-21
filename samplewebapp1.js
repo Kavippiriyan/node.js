43eca70d8659983eef0fb349fc214deb524007d1
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-21 16:16:09 +0530

    updated web_app.js 21/03/2024

--- samplewebapp1.js
+++ samplewebapp1.js
@@ -3,32 +3,27 @@ const http = require("http")
 const url = require("url")
 
 const server = http.createServer((req, res) => {
-
     const parsedurl = url.parse(req.url, true);
     console.log(parsedurl);
 
-
     if (parsedurl.pathname === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" })
+        res.write("<h1>Welcome Page</h1>")
+        res.end()
+    }
+    else if (parsedurl.pathname === "/home") {
         res.writeHead(200, { "Content-Type": "text/html" })
         res.write("<h1>Home Page</h1>")
         res.end()
     }
-    else if (parsedurl.pathname === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" })
-        res.write("<h1>About Page</h1>")
-        res.end()
-    }
-    else if (parsedurl.pathname === "/contact") {
-        res.writeHead(200, { "Content-Type": "text/html" })
-        res.write("<h1>Contact Page</h1>")
-        res.end()
-    }
-    else {
+    else 
+    {
         res.writeHead(404, { "Content-Type": "text/html" })
-        res.write("<h1>Page Not Found</h1>")
+        res.write("<h1>Page not found</h1>")
         res.end()
     }
+
 })
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+console.log("Server running on http://localhost:3000");
 })
