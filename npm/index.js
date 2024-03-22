f9c0b46f0a500bc7c7caaf52ea9d549c6d6565a1
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-22 15:35:10 +0530

    npm 22/03/2024

--- npm/index.js
+++ npm/index.js
@@ -2,5 +2,12 @@ const variable = require("lodash")
 
 variable.each([1,3,5],(item)=>
 {
-    console.log(item+2);
+    console.log(item+5);
 })
+
+/// lodash is one of the package 
+/// -- nodemon is one of the package is used for target one file whenever we are doing update on the file it will run
+// automatically
+
+// var a=10
+// console.log(a);
